18c9c80a20ef84459f68ed5605c02c512e3528cc
Siphesihle Nkosi <siphenkosii08@gmail.com>
2026-08-03 20:09:48 +0200

    data validation, and ensuring consistency across the values of each column

--- sql/cleaning.sql
+++ sql/cleaning.sql
@@ -1,5 +1,5 @@
 -- CLEANING --
-===================================
+-- ===================================
 
 -- CREATING A STAGING TABLE 
 SELECT *
@@ -157,7 +157,6 @@ FROM machine_failure_staging_deduplicated;
 
 SELECT *
 FROM machine_failure_staging_deduplicated;
-
 SELECT
 SUM(CASE WHEN air_temperature_k IS NULL THEN 1 ELSE 0 END) AS missing_air_temperature_k,
 SUM(CASE WHEN process_temperature_k IS NULL THEN 1 ELSE 0 END) AS missing_process_temperature_k,
@@ -171,3 +170,74 @@ SUM(CASE WHEN power_failure IS NULL THEN 1 ELSE 0 END) AS missing_power_failure,
 SUM(CASE WHEN overstrain_failure IS NULL THEN 1 ELSE 0 END) AS missing_overstrain_failure,
 SUM(CASE WHEN random_failure IS NULL THEN 1 ELSE 0 END) AS missing_random_failure
 FROM machine_failure_staging_deduplicated;
+
+
+
+-- VALIDATING DATA
+-- ====================
+
+-- CHECKING COLUMN VALUES
+SELECT DISTINCT machine_failure
+FROM machine_failure_staging_deduplicated;
+
+SELECT DISTINCT tool_wear_failure
+FROM machine_failure_staging_deduplicated;
+
+SELECT DISTINCT heat_dissipation_failure
+FROM machine_failure_staging_deduplicated;
+
+SELECT DISTINCT power_failure
+FROM machine_failure_staging_deduplicated;
+
+SELECT DISTINCT overstrain_failure
+FROM machine_failure_staging_deduplicated;
+
+SELECT DISTINCT random_failure
+FROM machine_failure_staging_deduplicated;
+
+-- dropping columns because they only contain 1 value
+
+ALTER TABLE machine_failure_staging_deduplicated
+DROP COLUMN heat_dissipation_failure;
+
+ALTER TABLE machine_failure_staging_deduplicated
+DROP COLUMN random_failure;
+
+-- CHECKING FOR IMPOSSIBLE VALUES
+
+SELECT *
+FROM machine_failure_staging_deduplicated
+WHERE air_temperature_k < 0
+OR process_temperature_k < 0
+OR rotational_speed_rpm < 0
+OR torque_nm < 0
+OR tool_wear_min < 0;
+
+-- changing impossible values to NULL
+
+UPDATE machine_failure_staging_deduplicated
+SET air_temperature_k = null
+WHERE air_temperature_k < 0;
+
+-- CHECKING FOR CONSISTENCY AND DATA QUALITY
+
+SELECT product_type, COUNT(*)
+FROM machine_failure_staging_deduplicated
+GROUP BY product_type;
+
+UPDATE machine_failure_staging_deduplicated
+SET product_type = TRIM(product_type);
+
+UPDATE machine_failure_staging_deduplicated
+SET product_type = 'M'
+WHERE product_type = 'medium';
+
+SELECT product_type, COUNT(*)
+FROM machine_failure_staging_deduplicated
+GROUP BY product_type;
+
+SELECT *
+FROM machine_failure_staging_deduplicated;
+
+
+
