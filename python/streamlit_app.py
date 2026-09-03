673b8ce17657a141875e4295f4744fe1d4a3a9d3
Siphesihle Nkosi <siphenkosii08@gmail.com>
2026-09-03 21:28:30 +0200

    finished stramlit skeleton and added tree models

--- python/streamlit_app.py
+++ python/streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Machine Failure Predictions")
 
@@ -17,5 +18,57 @@ with st.sidebar:
     )
     air_temperature_k = st.number_input (
         "Air Temperature K",
-        value = 300
+        value = 290.0
     )
+    process_temperature_k = st.number_input (
+            "Process Temperature K",
+            value = 300.0
+        )
+    rotational_speed_rpm = st.number_input (
+            "Rotational Speed RPM",
+            value = 1539.0
+        )
+    torque_nm = st.number_input (
+            "Torque NM",
+            value = 41.0
+        )
+    tool_wear_min = st.number_input (
+            "Tool Wear min",
+            value = 108
+        )
+    
+    # Features that were engineered 
+    machine_operating_process_k = (
+        process_temperature_k - air_temperature_k
+    )
+    machanical_power = (
+        torque_nm * rotational_speed_rpm
+    )
+
+# Table to display input values
+input_data = pd.DataFrame({
+        "Machine Input": [
+        "Prodct Type",
+        "Air Temperature K",
+        "Process Temperature K",
+        "Rotational Speed RPM",
+        "Torque NM",
+        "Tool Wear min"
+    ],
+
+    "Value": [
+        product_type,
+        air_temperature_k,
+        process_temperature_k,
+        rotational_speed_rpm,
+        torque_nm,
+        tool_wear_min
+    ]
+})
+st.dataframe(input_data)
+
+st.subheader("Prediction")
+predict_button = st.button("Predict Machine Failure",
+                           type="primary")
+if predict_button:
+    st.success("Prediction will appear here")
